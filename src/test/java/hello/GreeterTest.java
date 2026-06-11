9129112ab3209402fc092942842debdde286bcba
Vaishnavi Sapkal <sapkalvv2005@gmail.com>
2026-06-11 17:16:29 +0530

    Update GreeterTest.java

--- src/test/java/hello/GreeterTest.java
+++ src/test/java/hello/GreeterTest.java
@@ -1,17 +1,16 @@
 package hello;
 
 import static org.hamcrest.CoreMatchers.containsString;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 
 public class GreeterTest {
 
-  private Greeter greeter = new Greeter();
+    private Greeter greeter = new Greeter();
 
-  @Test
-  public void greeterSaysHello() {
-    assertThat(greeter.sayHello(), containsString("Bye"));
-  }
-
+    @Test
+    public void greeterSaysHello() {
+        assertThat(greeter.sayHello(), containsString("Hello world!"));
+    }
 }
